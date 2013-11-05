30b5ac2143af971a6ec25105bc6287539bf35f54
Ben Weitzman <benweitzman@gmail.com>
2013-11-05 16:25:00 -0500

    start work on network interface

--- src/Main.hs
+++ src/Main.hs
@@ -1,11 +1,17 @@
 import Pitch.Deck
 import Pitch.Card
 import Pitch.Game
+import Pitch.Network
 import System.Random
 import Control.Monad.State
 import Control.Monad
+import Control.Concurrent
+import System.IO
+import Network
 
 main = do
+  serverThreadID <- forkIO runServer
   gen <- getStdGen
   let gameState = mkGameState gen [Player (Human "Ben"), Player (Human "John"), Player (Human "Alex")]
   runStateT playGame gameState
+  
